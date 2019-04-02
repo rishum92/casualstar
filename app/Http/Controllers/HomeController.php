887068b544c9be807a467804041d21919c5b44d1
Pk8349965930 <Pankajkevat1994@gmail.com>
2019-04-02 15:53:04 +0530

    add code competition

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -108,8 +108,12 @@ class HomeController extends BaseController
       if(Auth::check()) {
         $user_id = Auth::user()->id;
         $getdata = competition_user::getdata();
+        
+        //echo '<pre>';print_r($getdata);exit;
         $exist = competition_user::existuser($user_id);
+        
         $voter_count = competition_user::vote_count($user_id);
+        //echo '<pre>';print_r($voter_count);
         $showdate= competition_user::showdate();
         $updatedate = $showdate[0]->ExpiryDate;
         $date_array = explode("-",$updatedate); // split the array
@@ -117,6 +121,9 @@ class HomeController extends BaseController
         $var_month = $date_array[1]; //month segment
         $var_day = $date_array[2]; //year segment
         $new_date_format = "$var_day/$var_month/$var_year";
+        $showtermscondition= competition_user::showtermscondition();
+        //$showtermscondition = nl2br($showtermscondition);
+        //echo '<pre>';print_r($showtermscondition);exit;
         
         return view('competitions',['competitionuser' =>$getdata,'exists'=>$exist,'showdate'=>$new_date_format,'termscondition'=>$showtermscondition,'voter_count'=>$voter_count]);
     } else {
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -159,6 +159,7 @@
 			Route::resource('photo', 'Api\PhotoController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 			Route::resource('photo-like', 'Api\PhotoLikeController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 			Route::resource('photo-comment', 'Api\PhotoCommentController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
+			Route::resource('profile-comment', 'Api\UserController@profile_comment');
 			Route::resource('privatePhoto', 'Api\PrivatePhotoController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 			Route::resource('favorite', 'Api\FavoriteController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 			Route::patch('photo-reorder', ['uses' => 'Api\PhotoController@reorder', 'as' => 'photo.reorder']);
@@ -216,8 +217,14 @@
 
 	//Competitions Controller  And Competition Date Routes
 	Route::post('competition-user','competitionController@add');
-	Route::get('popregister','competitionController@demo');
-	Route::get('competitiondelete/{id}','competitionController@delete');
+	Route::get('competitiondelete/{id}','competitionController@competitiondelete');
 	Route::post('editdate','competitionController@editd');
 	
 	Route::get('browseuser', ['uses' => 'SearchCompetitionUserController@browse', 'as' => 'views']);
+	Route::post('terms_store','competitionController@termsstore');
+	Route::post('competitioncomment','competitionController@storecomment');
+	Route::post('confirm_vote','competitionController@confirm_vote');
+	Route::get('expand_image/{id}','competitionController@expand_image');
+	Route::post('amount_edit','competitionController@amount_edit');
+	Route::resource('competition_user', 'CompetitionUserController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
+
