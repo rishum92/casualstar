5d4865815b04d6223a82b7650abd585608d35751
Pk8349965930 <Pankajkevat1994@gmail.com>
2019-04-02 13:26:10 +0530

    gg

--- storage/framework/views/035868f2c312acf26495dcaa195066ed324a4956.php
+++ storage/framework/views/035868f2c312acf26495dcaa195066ed324a4956.php
@@ -172,8 +172,14 @@
         <div class="wrap">
           <div id="userPhotoProfile"> 
             <div class="image" lightgallery data-src="[[getPhotoUrl(user.img)]]">
-              <a href="[[getPhotoUrl(user.img)]]" class="lightGallery" title="[[user.username]]">
+              <!-- <a href="[[getPhotoUrl(user.img)]]" class="lightGallery" title="[[user.username]]">
                 <img data-ng-src="[[getPhotoPreviewUrl(user.img)]]" alt="profile pic" />
+              </a> -->
+              <a href="[[getPhotoUrl(user.img)]]" class="lightGallery" title="[[user.username]]">
+                <img ng-if="[[getPhotoPreviewUrl(user.img == '')]]" src="img/male.jpg" alt="profile pic" />
+              </a>
+               <a href="[[getPhotoUrl(user.img)]]" class="lightGallery" title="[[user.username]]">
+                <img ng-if="[[getPhotoPreviewUrl(user.img != '')]]" data-ng-src="[[getPhotoPreviewUrl(user.img)]]" alt="profile pic" />
               </a>
             </div>
 			<?php if(Auth()->user()->title!="ADMIN"): ?>
@@ -353,10 +359,13 @@
         </ui-select>
       </div>
     </div>
-    <br>
-    
     <?php echo $__env->make('modals.donate', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
   </div>
+  <br>
+  <center>
+    <button style = "background-color: #f21d84; color:white; padding:10px 25px;">Access ALL Private    Galleries
+    </button>
+  </center>
 
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
