36935685beb1e7b740115ca00cba7106bb05712d
rishum92 <rishum92@gmail.com>
2019-04-05 14:19:01 +0530

    test

--- storage/framework/views/3c7fa689fe551c6f34147cd6c30713f3e7915838.php
+++ storage/framework/views/3c7fa689fe551c6f34147cd6c30713f3e7915838.php
@@ -254,6 +254,8 @@
 	 <?php endif; ?>
 	
   </div>
+      
 
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
